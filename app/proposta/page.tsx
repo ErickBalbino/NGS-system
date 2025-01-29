9dd375acdaf8585e1957249537f503ee9d7381ae
ErickBalbino <erickbaddog2@gmail.com>
2025-01-29 14:09:43 -0300

    .

--- app/proposta/page.tsx
+++ app/proposta/page.tsx
@@ -1,16 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import { ProposalForm } from "@/components/forms/form";
-import { Header } from "@/components/headers/header";
-import PrivateRoute from "@/components/private/PrivateRoute";
+import { ProposalForm } from '@/components/forms/form';
+import { Header } from '@/components/headers/header';
+import PrivateRoute from '@/components/private/PrivateRoute';
 
 export default async function Proposta() {
   return (
-    <PrivateRoute>
-      <div className="flex flex-col mb-5 w-[100%]">
-        <Header activeTab="proposta" />
-        <ProposalForm />
-      </div>
-    </PrivateRoute>
+    <div className="flex flex-col mb-5 w-[100%]">
+      <Header activeTab="proposta" />
+      <ProposalForm />
+    </div>
   );
 }
