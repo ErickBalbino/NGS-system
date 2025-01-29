ec425aafb5fd1cea8ec685573ea38049e484b3cc
ErickBalbino <erickbaddog2@gmail.com>
2025-01-29 14:10:41 -0300

    .

--- app/proposta/page.tsx
+++ app/proposta/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { ProposalForm } from '@/components/forms/form';
 import { Header } from '@/components/headers/header';
-import PrivateRoute from '@/components/private/PrivateRoute';
 
 export default async function Proposta() {
   return (
